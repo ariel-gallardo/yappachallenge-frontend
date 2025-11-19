a666a3706d5a7fac2bd6794a5e0a246f208e8a75
Ariel Gallardo <ariel.gallardo.dev@gmail.com>
2025-11-19 18:16:13 -0300

    feat(clientes): add ClientesModule and List component
    - Create ClientesModule with CommonModule import
    - Add new List component (HTML, CSS, TS)
    - Register List component inside ClientesModule
    - Scaffold basic template and empty stylesheet

---
+++ src/features/clientes/clientes-module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { List } from './list/list';
+
+
+
+@NgModule({
+  declarations: [
+    List
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ClientesModule { }
